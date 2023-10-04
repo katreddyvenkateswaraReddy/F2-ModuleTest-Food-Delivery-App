8ddbd62f67ba56c2366acbead770c3b23062bb1c
katreddyvenkateswaraReddy <63310358+katreddyvenkateswaraReddy@users.noreply.github.com>
2023-10-04 09:55:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function () {
     },
     {
       name: "Paneer Tikka",
-      imageSrc: "https://images.unsplash.com/photo-1567188040759-fb8a883dc6d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1917&q=80",
+      imageSrc: "https://source.unsplash.com/random?paneertikka",
       time: "40 min",
       type: "veg",
       isLiked: false,
